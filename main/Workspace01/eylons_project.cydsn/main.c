c66b569068002bb0bffde0f9f17a3dba600c8532
eylonk12 <eylonkapel12@gmail.com>
2022-04-23 14:34:44 +0300

    Changed the program for PSoC

--- main/Workspace01/eylons_project.cydsn/main.c
+++ main/Workspace01/eylons_project.cydsn/main.c
@@ -28,6 +28,7 @@ const int min_vdac_power = 0;
 
 const int always_enable_leds = 1; 
 
+
 int main(void)
 {
     // Starting the DAC used for leds display of magnets location
@@ -47,9 +48,6 @@ int main(void)
 
     Opamp_1_Start();
     Opamp_1_Init();
-    
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-
     
     for(;;)
     {
@@ -72,10 +70,7 @@ int main(void)
                 pulse_out_Write(~ pulse_out_Read());
                 if (first_moving_state == 1){
                     current_delay = start_delay;
-                    first_moving_state = 0;
-                } else{
-                    // The magnet is on the left sensor and needs to move to the right one.
-                    VDAC8_1_SetValue(max_vdac_power);
+                } else{ // The magnet is on the left sensor and needs to move to the right one.
                     if (current_step < total_steps){
                         VDAC8_1_SetValue(max_vdac_power - (int)(current_step*max_vdac_power/total_steps));
                         if (current_step > total_steps - 50){
@@ -84,19 +79,18 @@ int main(void)
                             leds_enable_Write(1);
                         }
                         
-                        current_delay = abs(725-current_step);
+                        current_delay = abs((total_steps/2)-current_step);
                         if (current_delay > min_speed_delay){
                             current_delay = min_speed_delay;
                         }
                         if (current_delay < max_speed_delay){
                             current_delay = max_speed_delay;
                         }
-                        
-
                         CyDelayUs(current_delay);
-                    } else{ // Passed the total steps
-                        
+                    } else{ // Passed the total steps, we assume that if we reach here we are close to the sample.
+                        CyDelayUs(min_speed_delay);
                     }
+                    current_step++;
                     if (sensor_right_Read() == 0){
                         current_step = 0;
                         state = stay_on_sample;
@@ -105,116 +99,54 @@ int main(void)
               break;
             
             case stay_on_sample:
+                if (direction_out_Read() == from_sample){
+                    first_moving_state = 0;
+                    state = move_from_sample;
+                }
               break;
             
             case move_from_sample:
-                
-              first_moving_state = 0;
+                direction_out_Write(from_sample);
+                pulse_out_Write(~ pulse_out_Read());
+                if (first_moving_state == 1){
+                    current_delay = start_delay;
+                } else{ // The magnet is on the right sensor and needs to move to the left one.
+                    if (current_step < total_steps){
+                        VDAC8_1_SetValue((int)(current_step*max_vdac_power/total_steps));
+                        if (current_step < 50){
+                            leds_enable_Write(always_enable_leds);
+                        } else{
+                            leds_enable_Write(1);
+                        }
+                        
+                        current_delay = abs((total_steps/2)-current_step);
+                        if (current_delay > min_speed_delay){
+                            current_delay = min_speed_delay;
+                        }
+                        if (current_delay < max_speed_delay){
+                            current_delay = max_speed_delay;
+                        }
+                        CyDelayUs(current_delay);
+                    } else{ // Passed the total steps, we assume that if we reach here we are close to the sample.
+                        CyDelayUs(min_speed_delay);
+                    }
+                    current_step++;
+                    if (sensor_left_Read() == 0){
+                        current_step = 0;
+                        state = stay_off_sample;
+                    }
+                }
               break;
             
             case stay_off_sample:
-              // statements
+              if (direction_out_Read() == to_sample){
+                    first_moving_state = 0;
+                    state = move_from_sample;
+                }
               break;
         }
     }
-    
-    
-    
-    
-    
-    
-    for(;;)
-    {
-        if (operation_input_Read() == 0)
-        {
-            if (sensor_left_Read() == 1)
-            {
-                if (sensor_right_Read() == 0)
-                {
-                    // The magnet is on the right sensor (sample) and needs to move to the left one.
-                    direction_out_Write(0);
-                    VDAC8_1_SetValue(0);
-                    for (int i=0;i<total_steps;i++)
-                    {
-                        VDAC8_1_SetValue((int)(i*max_vdac_power/total_steps));
-                        if (i < 50)
-                            leds_enable_Write(0 + always_enable_leds);
-                        else
-                            leds_enable_Write(1);
-                            
-                            
-                        if (sensor_left_Read() == 0)
-                            break;
-                        pulse_out_Write(~ pulse_out_Read());
-                        current_delay = abs(725-i);
-                        if (current_delay > min_speed_delay)
-                            current_delay = min_speed_delay;
-                        if (current_delay < max_speed_delay)
-                            current_delay = max_speed_delay;
-                        
-                        CyDelayUs(current_delay);
-                    }
-                }
-                else // sensor_right_Read() == 1
-                {
-                    // The magnet is in the middle and needs to move to the left sensor.
-                    direction_out_Write(operation_input_Read());
-                    pulse_out_Write(~ pulse_out_Read());
-                    CyDelayUs(start_delay);
-                }
-            }
-            else // sensor_left_Read() == 0
-            // The magnet is on the left.
-            {
-                VDAC8_1_SetValue(max_vdac_power);
-            }
-        }
-        else // operation_input_Read() == 1.
-        {
-            if (sensor_right_Read() == 1)
-            {
-                if (sensor_left_Read() == 0)
-                {
-                    // The magnet is on the left sensor and needs to move to the right one.
-                    direction_out_Write(1);
-                    VDAC8_1_SetValue(max_vdac_power);
-                    for (int i=0;i<total_steps;i++)
-                    {
-                        VDAC8_1_SetValue(max_vdac_power - (int)(i*max_vdac_power/total_steps));
-                        if (i > total_steps - 50)
-                            leds_enable_Write(0 + always_enable_leds);
-                        else
-                            leds_enable_Write(1);
-                            
-                        if (sensor_right_Read() == 0)
-                            break;
-                        pulse_out_Write(~ pulse_out_Read());
-                        current_delay = abs(725-i);
-                        if (current_delay > min_speed_delay)
-                            current_delay = min_speed_delay;
-                        if (current_delay < max_speed_delay)
-                            current_delay = max_speed_delay;
-                        
-                        CyDelayUs(current_delay);
-                    }
-                }
-                else
-                {
-                    // The magnet is in the middle and needs to move to the right sensor.
-                    // direction_out_Write(operation_input_Read());
-                    direction_out_Write(operation_input_Read());
-                    pulse_out_Write(~ pulse_out_Read());
-                    CyDelayUs(start_delay);
-                }
-            }
-            else 
-            // The magnet is on the smaple.
-            {
-                VDAC8_1_SetValue(0);
-                leds_enable_Write(0 + always_enable_leds);
-            }
-        }
-    }
 }
+ 
 
 /* [] END OF FILE */
